4788d036ad924abc2a03982dc81a2a410c798834
Yaroslava <glominor@yandex.ru>
2018-07-05 11:33:43 +1000

    Add_changes

--- application/core/route.php
+++ application/core/route.php
@@ -8,85 +8,74 @@
 class Route
 {
 
-	static function start()
-	{
-		// контроллер и действие по умолчанию
-		$controller_name = 'Main';
-		$action_name = 'index';
-		
-		$routes = explode('/', $_SERVER['REQUEST_URI']);
+	  function start()
+  {
+    $model = 'Product';
+    $controller_name = 'List';
+    $action_name = 'index';
+    $action_parameter = '';
 
-		// получаем имя контроллера
-		if ( !empty($routes[1]) )
-		{	
-			$controller_name = $routes[1];
-		}
-		
-		// получаем имя экшена
-		if ( !empty($routes[2]) )
-		{
-			$action_name = $routes[2];
-		}
+    $routes = explode('/', $_SERVER['REQUEST_URI']);
+//    var_dump($routes);
 
-		// добавляем префиксы
-		$model_name = 'Model_'.$controller_name;
-		$controller_name = 'Controller_'.$controller_name;
-		$action_name = 'action_'.$action_name;
-
-		/*
-		echo "Model: $model_name <br>";
-		echo "Controller: $controller_name <br>";
-		echo "Action: $action_name <br>";
-		*/
-
-		// подцепляем файл с классом модели (файла модели может и не быть)
-
-		$model_file = strtolower($model_name).'.php';
-		$model_path = "application/models/".$model_file;
-		if(file_exists($model_path))
-		{
-			include "application/models/".$model_file;
-		}
-
-		// подцепляем файл с классом контроллера
-		$controller_file = strtolower($controller_name).'.php';
-		$controller_path = "application/controllers/".$controller_file;
-		if(file_exists($controller_path))
-		{
-			include "application/controllers/".$controller_file;
-		}
-		else
-		{
-			/*
-			правильно было бы кинуть здесь исключение,
-			но для упрощения сразу сделаем редирект на страницу 404
-			*/
-			Route::ErrorPage404();
-		}
-		
-		// создаем контроллер
-		$controller = new $controller_name;
-		$action = $action_name;
-		
-		if(method_exists($controller, $action))
-		{
-			// вызываем действие контроллера
-			$controller->$action();
-		}
-		else
-		{
-			// здесь также разумнее было бы кинуть исключение
-			Route::ErrorPage404();
-		}
-	
-	}
-
-	function ErrorPage404()
-	{
-        $host = 'http://'.$_SERVER['HTTP_HOST'].'/';
-        header('HTTP/1.1 404 Not Found');
-		header("Status: 404 Not Found");
-		header('Location:'.$host.'404');
+    if (!empty($routes[1])) {
+      $controller_name = $routes[1];
     }
+    if (!empty($routes[2])) {
+      if (!!is_numeric($routes[2])) {
+        $action_parameter = $routes[2];
+      } else {
+        $action_name = $routes[2];
+      }
+    }
+
+    $model_name = $model . '_Model';
+    $controller_name = $controller_name . '_Controller';
+    $action_name = 'action_' . $action_name;
+
+    $model_file = strtolower($model_name) . '.php';
+    $model_path = 'app/models/' . $model_file;
+
+    if (file_exists($model_path)) {
+      include $model_path;
+    }
+
+    $controller_file = strtolower($controller_name) . '.php';
+    $controller_path = 'app/controllers/' . $controller_file;
+    if (!!file_exists($controller_path)) {
+      include $controller_path;
+      if (strtolower($controller_name) === '404_controller') {
+        $controller_name = 'NotFound_Controller';
+      }
+      $controller = new $controller_name($this->entityManager);
+      $action = $action_name;
+
+//      if (method_exists($controller, $action($action_parameter))) {
+//        if (strlen($action_parameter) > 0) {
+//          $controller->$action($action_parameter);
+//        } else {
+//          $controller->$action(null);
+//        }
+//      } else {
+//        $this->ErrorPage404();
+//      }
+      if (strlen($action_parameter) > 0) {
+        $controller->$action($action_parameter);
+      } else {
+        $controller->$action(null);
+      }
+    } else {
+      $this->ErrorPage404();
+    }
+
+  }
+
+  private function ErrorPage404()
+  {
+    $host = 'http://' . $_SERVER['HTTP_HOST'] . '/';
+    header('HTTP/1.1 404 Not Found');
+    header('Status: 404 Not Found');
+    header('Location: ' . $host . '404');
+  }
     
 }
